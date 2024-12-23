033fcd6181ccac6f097f8c088a436a2a0eec43bd
Caedis <Caedis@users.noreply.github.com>
2024-12-22 20:31:42 -0600

    also add here to be consistant

--- src/main/java/com/cleanroommc/bogosorter/common/network/SReloadConfig.java
+++ src/main/java/com/cleanroommc/bogosorter/common/network/SReloadConfig.java
@@ -3,6 +3,8 @@ package com.cleanroommc.bogosorter.common.network;
 import com.cleanroommc.bogosorter.common.SortConfigChangeEvent;
 import com.cleanroommc.bogosorter.common.config.PlayerConfig;
 import com.cleanroommc.bogosorter.common.config.Serializer;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.common.MinecraftForge;
@@ -19,6 +21,7 @@ public class SReloadConfig implements IPacket {
     public void decode(PacketBuffer buf) throws IOException {
     }
 
+    @SideOnly(Side.CLIENT)
     @Override
     public IPacket executeClient(NetHandlerPlayClient handler) {
         Serializer.loadConfig();
