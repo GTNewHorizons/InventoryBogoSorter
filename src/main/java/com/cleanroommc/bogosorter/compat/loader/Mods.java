c628b1b6f3e0270417f33c02e9233da177ede4d4
Caedis <Caedis@users.noreply.github.com>
2025-01-03 16:29:01 +0000

    Split up GT5u and GT6 checks (#24)

--- src/main/java/com/cleanroommc/bogosorter/compat/loader/Mods.java
+++ src/main/java/com/cleanroommc/bogosorter/compat/loader/Mods.java
@@ -2,6 +2,8 @@ package com.cleanroommc.bogosorter.compat.loader;
 
 import cpw.mods.fml.common.Loader;
 
+import java.util.function.Supplier;
+
 public enum Mods {
 
     EnderStorage("EnderStorage"),
@@ -10,7 +12,8 @@ public enum Mods {
     CookingForBlockheads("cookingforblockheads"),
     Ae2("appliedenergistics2"),
     Forestry("Forestry"),
-    GT5u("gregtech"),
+    GT5u(() -> Loader.isModLoaded("gregtech") && !Loader.isModLoaded("gregapi")),
+    GT6(() -> Loader.isModLoaded("gregtech") && Loader.isModLoaded("gregapi")),
     Backpack("Backpack"),
     GalacticraftCore("galacticraftcore"),
     AdventureBackpack2("adventurebackpack"),
@@ -18,15 +21,26 @@ public enum Mods {
     Tconstruct("TConstruct"),;
 
     public final String modid;
+    private final Supplier<Boolean> supplier;
     private Boolean loaded;
 
     Mods(String modid) {
         this.modid = modid;
+        this.supplier = null;
+    }
+    Mods(Supplier<Boolean> supplier) {
+        this.supplier = supplier;
+        this.modid = null;
+
     }
 
     public boolean isLoaded() {
         if (loaded == null) {
-            loaded = Loader.isModLoaded(modid);
+            if (supplier != null){
+                loaded = supplier.get();
+            }else if (modid != null) {
+                loaded = Loader.isModLoaded(modid);
+            } else loaded = false;
         }
         return loaded;
     }
