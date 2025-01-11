148331514556ce355f9c0f8c274f9e8d88769b2f
Abdulaziz <127992888+0hwx@users.noreply.github.com>
2025-01-11 08:34:18 +0300

    Update Mods.java

--- src/main/java/com/cleanroommc/bogosorter/compat/loader/Mods.java
+++ src/main/java/com/cleanroommc/bogosorter/compat/loader/Mods.java
@@ -19,7 +19,7 @@ public enum Mods {
     AdventureBackpack2("adventurebackpack"),
     ProjectE("ProjectE"),
     Tconstruct("TConstruct"),
-    ServerUtilities("serverutilities");
+    ServerUtilities("serverutilities"),
     Nutrition("nutrition"),
     Bibliocraft("BiblioCraft"),
     Mekanism("Mekanism"),
@@ -31,8 +31,6 @@ public enum Mods {
     Etfuturum("etfuturum"),
     IronChest("IronChest"),
     IC2("IC2");
-   
-
 
     public final String modid;
     private final Supplier<Boolean> supplier;
