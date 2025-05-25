880fd4f03e4322d58839c8892a5590a74ada46c8
tyrannus <60930117+tyrannus00@users.noreply.github.com>
2025-05-25 20:45:05 +0200

    Fixed right click crafting (#84)
    Co-authored-by: tyrannus <noneofyour@busi.ness>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Abdulaziz <127992888+0hwx@users.noreply.github.com>

--- src/main/java/yalter/mousetweaks/Main.java
+++ src/main/java/yalter/mousetweaks/Main.java
@@ -119,13 +119,13 @@ public class Main {
         if (mouseState.isButtonPressed(MouseButton.RIGHT)) {
             if (!oldRMBDown) firstRightClickedSlot = selectedSlot;
 
-            if (MTConfig.rmbTweak && handler.disableRMBDraggingFunctionality()) {
+            if (MTConfig.rmbTweak && firstRightClickedSlot != null
+                && !handler.isCraftingOutput(firstRightClickedSlot)
+                && handler.disableRMBDraggingFunctionality()) {
                 // Check some conditions to see if we really need to click the first slot.
-                if (firstRightClickedSlot != null
-                    // This condition is here to prevent double-clicking.
-                    && (firstRightClickedSlot != selectedSlot || oldSelectedSlot == selectedSlot)
-                    && !handler.isIgnored(firstRightClickedSlot)
-                    && !handler.isCraftingOutput(firstRightClickedSlot)) {
+                // This condition is here to prevent double-clicking.
+                if ((firstRightClickedSlot != selectedSlot || oldSelectedSlot == selectedSlot)
+                    && !handler.isIgnored(firstRightClickedSlot)) {
                     ItemStack targetStack = firstRightClickedSlot.getStack();
                     ItemStack stackOnMouse = Minecraft.getMinecraft().thePlayer.inventory.getItemStack();
 
@@ -199,7 +199,6 @@ public class Main {
                 }
             }
         }
-
         handleWheel(selectedSlot);
     }
 
